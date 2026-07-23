d1707fe7617b1c8879d63b90e38ec532ba629999
the critter <52157258+SaltySchnauzer@users.noreply.github.com>
2026-07-23 14:39:56 +0800

    Minor fixes

--- Scripts/countdown.gd
+++ Scripts/countdown.gd
@@ -3,7 +3,7 @@ class_name Countdown extends Node
 @export var display_name: String = "Un-named Timer"
 var time_max: float = 60. ## Maximum amount of time per clock
 var time_left: float = 10.
-
+var active: bool = true
 signal finished()
 
 # Called when the node enters the scene tree for the first time.
@@ -12,10 +12,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if !active: pass
 	time_left -= delta
 	if time_left < 0:
 		finish()
 		
 func finish():
+	active = false
 	print("%s has finished!" % display_name)
 	finished.emit()
