9b1e85b46316f387cfb6e4827b0ff1a214bdd832
the critter <52157258+SaltySchnauzer@users.noreply.github.com>
2026-07-23 14:15:49 +0800

    Map + Prelim setup
    - Thanks to Zac for shadercode for map

---
+++ Scripts/countdown.gd
@@ -0,0 +1,21 @@
+class_name Countdown extends Node
+
+@export var display_name: String = "Un-named Timer"
+var time_max: float = 60. ## Maximum amount of time per clock
+var time_left: float = 10.
+
+signal finished()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	time_left -= delta
+	if time_left < 0:
+		finish()
+		
+func finish():
+	print("%s has finished!" % display_name)
+	finished.emit()
