76afca11f334d4c50f167cdf1b9fbe385d92d80c
rbcarrots <redbluecarrots@gmail.com>
2026-07-24 22:09:18 +0800

    unstable orbit

--- Scripts/event.gd
+++ Scripts/event.gd
@@ -4,15 +4,18 @@ var old_pos : Vector2
 @export var icon := ""
 @export var minigame_scene := ""
 var is_open = false
+@onready var new_scene : PackedScene = load("res://Scenes/"+minigame_scene+".tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	old_pos = position
 	#$Icon/Sprite2D.texture = load("res://Assets/Art/Icons/" + icon)
-	var new_scene : PackedScene = load("res://Scenes/"+minigame_scene+".tscn")
+
+func create_minigame():
 	var minigame : Minigame = new_scene.instantiate()
 	$Panel/SubViewportContainer/SubViewport/SceneSlot.add_child(minigame)
 	minigame.connect("finished", close)
+	minigame.connect("failed", fail_close)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,6 +29,7 @@ func _on_icon_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> 
 		open()
 
 func open():
+	create_minigame()
 	is_open = true
 	$AnimationPlayer.play("FadeIn")
 	var tx = clamp(position.x, -320+96, 320-96)
@@ -40,3 +44,7 @@ func close():
 	var tw = get_tree().create_tween()
 	tw.tween_property(self, "position", old_pos, 0.3)
 	tw.play()
+	$Panel/SubViewportContainer/SubViewport/SceneSlot.get_child(0).queue_free()
+
+func fail_close():
+	close()
