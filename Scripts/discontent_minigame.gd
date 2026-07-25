2208037cf5289a07c14e19a6067199c6d01995fe
Joshua Noble <joshuarachpaul@hotmail.com>
2026-07-25 22:31:25 +0800

    feat: Discount Minigame Complete
    Added:
    - Random sentences selected.
    - User must copy the provided sentence.
    - Each letter the user gets right is highlighted.
    - 2 sentences in one minigame for now.

--- Scripts/discontent_minigame.gd
+++ Scripts/discontent_minigame.gd
@@ -1,11 +1,31 @@
 extends Minigame
 
-var count
+@export var sentences = 2
+var sentence = ""
+var comp_sentence = ""
+var written = ""
+var pos = 0
+var space = 0
+var last_index = 0
+
+var sentence_list = [
+	"There have been reports of adib staring", 
+	"Hello everyone nice to see you", 
+	"Do not panic everything is under control", 
+	"I am here for you if you need anything", 
+	"We will get through this together", 
+	"I understand your concerns but remain calm", 
+	"Remember the mission we will get through this",
+	"Sometimes sacrifices must be made", 
+	"Do not worry we are still in control", 
+	"Please be calm and do not yell",
+	"I promise everything will be ok"
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	%input.call_deferred("grab_focus")
+	_generate_sentence()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,4 +33,43 @@ func _process(delta: float) -> void:
 
 
 func _on_input_text_changed(new_text: String) -> void:
-	count += len(%input.text)
+	written = %input.text
+
+	for i in range(pos, len(new_text)):
+		if written[i] == comp_sentence[i]:
+			print(sentence[i])
+			if sentence[i + space] == " ":
+				print("space detected")
+				space += 1
+			var new_sentence = sentence.insert(space + i + 1, "[/color]") 
+			new_sentence = new_sentence.insert(0, "[color=green]") 
+			%words.text = new_sentence
+			print(new_sentence)
+			pos += 1
+		else:
+			# wrong lol
+			print("lose")
+			finish()
+			break
+		
+	if pos >= len(comp_sentence):
+		if sentences <= 0:
+			print("win")
+			finish()
+		else:
+			_generate_sentence()
+
+
+func _generate_sentence():
+	sentences -= 1
+	pos = 0
+	space = 0
+	%input.text = ""
+	var index = randi() % len(sentence_list)
+	if index == last_index:
+		index = (index + 1) % len(sentence_list)
+	else:
+		last_index = index
+	sentence = sentence_list[index]
+	comp_sentence = sentence.replace(" ", "").to_lower()
+	%words.text = sentence
