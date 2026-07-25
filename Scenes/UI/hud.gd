8c91b58a4065ec0b6da71152cc3b9cc62b407b52
rbcarrots <redbluecarrots@gmail.com>
2026-07-25 23:42:52 +0800

    timings and changes

--- Scenes/UI/hud.gd
+++ Scenes/UI/hud.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	show_warning("SATELLITE   ORBIT   UNSTABLE:   DOOMSDAY   IMMINENT")
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
