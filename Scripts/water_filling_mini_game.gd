ada469fed70837e5a91a1d99ac109bd41fc1f04a
the critter <52157258+SaltySchnauzer@users.noreply.github.com>
2026-07-26 02:23:41 +0800

    Reworked and Art'ed Dehydration game
    - Art provided
    - Game code reworked to allow for easy designer interaction
    - Input handled through `_unhandled_input` now - should be more reliable with other elements

--- Scripts/water_filling_mini_game.gd
+++ Scripts/water_filling_mini_game.gd
@@ -1,52 +1,65 @@
 extends Minigame
 
 # Nodes to call in the script
-@onready var water_node = get_node("Game/Water")
-@onready var water_fill = get_node("Game/WaterFill")
-@onready var key_node = get_node("Game/Key To Press")
+@export var z_key: Node2D
+@export var x_key: Node2D
+@export var waterlevel: Sprite2D
+
+## @DESIGNERS SEE THESE
+const SECONDS_PER_CORRECT: float = 5 ## Seconds of water added per correct key pressed
+const SECONDS_PER_INCORRECT: float = 0 ## Seconds of water deducted per incorrect (but possible) key pressed
+var seconds_max = 60. ## Maximum seconds of water possible in tank
+var seconds_left = 30.0 ## Amount of seconds of water left in tank
 
 # List for the keys mashing
 var keys_to_press = ["PressZ", "PressX"]
 
-# Highest point of the water
-var water_max = 200
-
 # Variable to hold which key to press
 var press_key
-var not_press_key
 
-var seconds_left = 30.0
+signal correct
+signal incorrect
 
 # Function to call the random key selection
-func random_key_press():
-	press_key = randi_range(0, 1)
-	
-	if press_key == 1:
-		not_press_key = 0
-	elif press_key == 0:
-		not_press_key = 1
-
-	key_node.text = keys_to_press[press_key][-1]
+func prepare_next_key():
+	press_key = randi_range(0, keys_to_press.size() - 1)
+	z_key.visible = press_key == 0
+	x_key.visible = press_key == 1
 
 func _ready() -> void:
-	random_key_press()
+	prepare_next_key()
+	correct.connect(correct_keypress)
+	incorrect.connect(incorrect_keypress)
 
 func _process(delta: float) -> void:
-	water_fill.text = str(snapped(seconds_left, 1)) + " Seconds"
-	
-	seconds_left = get_parent().get_parent().get_node("Timer").time_left
-	
-	water_node.size.y = lerp(water_node.size.y, seconds_left * 6, pow(0.1, delta))
-	
-	if Input.is_action_just_pressed(keys_to_press[press_key]): # Function restarts when key is pressed
-		random_key_press()
-		
-		seconds_left += 4 * 25 * 1.1 * delta
-	
-	elif Input.is_action_just_pressed(keys_to_press[not_press_key]):
-		seconds_left -= 5 * 15 * 1 * delta
-	
-	if seconds_left >= 60:
+	# Visual indicators
+	var target_position = 59 * (1 - seconds_left/seconds_max)
+	waterlevel.position.y = lerp(waterlevel.position.y, target_position, delta * 10)
+	seconds_left -= delta
+	# Close out if failed/succeeded
+	if seconds_left >= seconds_max:
 		finish()
 	elif seconds_left <= 0:
 		fail()
+		
+func _unhandled_input(event: InputEvent) -> void:
+	# Don't process if key is held down
+	if event.is_echo(): return
+	# Cycle through keys we care about
+	for i in keys_to_press.size():
+		var success_key = false
+		if i == press_key:
+			success_key = true
+		if event.is_action_pressed(keys_to_press[i]):
+			if success_key:
+				correct.emit()
+			else:
+				incorrect.emit()
+
+func correct_keypress():
+	prepare_next_key()
+	seconds_left = min(seconds_left + SECONDS_PER_CORRECT, seconds_max)
+
+func incorrect_keypress():
+	prepare_next_key()
+	seconds_left -= SECONDS_PER_INCORRECT
