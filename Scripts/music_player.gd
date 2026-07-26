0b6a36ea10e793f3eeb9545137c1fcd5e2e94a7f
kaojb-bass <jacobpran147@gmail.com>
2026-07-27 01:14:36 +0800

    fixed

--- Scripts/music_player.gd
+++ Scripts/music_player.gd
@@ -6,12 +6,20 @@ var audio_stream = self.stream as AudioStreamSynchronized
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	audio_stream.set_sync_stream_volume(0, 0)
+	audio_stream.set_sync_stream_volume(1, 0)
+	audio_stream.set_sync_stream_volume(2, -60)
+	audio_stream.set_sync_stream_volume(3, -60)
+	audio_stream.set_sync_stream_volume(4, -60)
+	audio_stream.set_sync_stream_volume(5, -60)
+	audio_stream.set_sync_stream_volume(6, -60)
+	audio_stream.set_sync_stream_volume(7, -60)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if !playing:
+		play()
 
 
 func _on_main_game_open(nam: Variant) -> void:
